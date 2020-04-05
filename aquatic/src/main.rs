af8b9d05dfa69974bec463299a59d5f1080e9ec2
Joakim Frostegård <joakim.frostegard@gmail.com>
2020-04-05 03:18:56 +0200

    main: add "socket_timeout" var

--- aquatic/src/main.rs
+++ aquatic/src/main.rs
@@ -9,17 +9,18 @@ use common::State;
 
 fn main(){
     let addr = ([127, 0, 0, 1], 3000).into();
-    let socket = network::create_socket(addr, 4096 * 8);
     let state = State::new();
+    let socket = network::create_socket(addr, 4096 * 8);
+    let socket_timeout = Duration::from_millis(1000);
 
     for i in 1..4 {
         let socket = socket.try_clone().unwrap();
         let state = state.clone();
 
         ::std::thread::spawn(move || {
-            network::run_event_loop(state, socket, i, Duration::from_millis(1000));
+            network::run_event_loop(state, socket, i, socket_timeout);
         });
     }
 
-    network::run_event_loop(state, socket, 0, Duration::from_millis(1000));
+    network::run_event_loop(state, socket, 0, socket_timeout);
 }
