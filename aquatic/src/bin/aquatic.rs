9d66a5e7d08c0aaccb14c8a53bfe264625cfc6f7
Joakim Frostegård <joakim.frostegard@gmail.com>
2020-04-06 02:42:23 +0200

    Use mimalloc instead of system allocator

--- aquatic/src/bin/aquatic.rs
+++ aquatic/src/bin/aquatic.rs
@@ -1,5 +1,10 @@
 use aquatic;
 
+
+#[global_allocator]
+static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
+
+
 fn main(){
     aquatic::run();
 }
--- aquatic/src/bin/bench_handlers/main.rs
+++ aquatic/src/bin/bench_handlers/main.rs
@@ -13,6 +13,10 @@ mod connect;
 mod scrape;
 
 
+#[global_allocator]
+static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
+
+
 fn main(){
     connect::bench();
 
