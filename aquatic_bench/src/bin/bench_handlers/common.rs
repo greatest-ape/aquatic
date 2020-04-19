88feb49c3d16b6825da5e8350a1e356e1a9a2659
Joakim Frostegård <joakim.frostegard@gmail.com>
2020-04-19 10:38:59 +0200

    aquatic_bench: fix progress bar min width for request type info

--- aquatic_bench/src/bin/bench_handlers/common.rs
+++ aquatic_bench/src/bin/bench_handlers/common.rs
@@ -7,7 +7,7 @@ pub const NUM_INFO_HASHES: usize = 10_000;
 
 
 pub fn create_progress_bar(name: &str, iterations: u64) -> ProgressBar {
-    let t = format!("{:<16} {}", name, "{wide_bar} {pos:>2}/{len:>2}");
+    let t = format!("{:<8} {}", name, "{wide_bar} {pos:>2}/{len:>2}");
     let style = ProgressStyle::default_bar().template(&t);
 
     ProgressBar::new(iterations).with_style(style)
