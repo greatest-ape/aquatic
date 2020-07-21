b3154c5bcfd72ea53ccaacb4e5fc6936b1167d50
Joakim Frostegård <joakim.frostegard@gmail.com>
2020-07-21 14:25:47 +0200

    aquatic http protocol: delete obsoleted request-from-path benchmark
    Now there is a request-from-bytes benchmark that supersedes
    request-from-path.
    
    Final comparison of gains made since first benchmark:
    
    request-from-path
        time:   [540.01 ns 541.51 ns 543.15 ns]
        change: [-90.786% -90.741% -90.696%] (p = 0.00 < 0.01)
        Performance has improved.

--- scripts/criterion/aquatic-http-request-from-path.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Compare against latest. If you commit changes, replace "latest" directory
-# with "new" directory after running benchmark.
-
-export RUSTFLAGS="-C target-cpu=native"
-
-cargo bench --bench bench_request_from_path -- --noplot --baseline latest
