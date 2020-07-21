c1b2a308031008ca0c3b0643eff5f659054be276
Joakim Frostegård <joakim.frostegard@gmail.com>
2020-07-21 15:59:32 +0200

    criterion request-from-bytes script: ask user about saving result

--- scripts/criterion/aquatic-http-request-from-bytes.sh
+++ scripts/criterion/aquatic-http-request-from-bytes.sh
@@ -1,7 +1,19 @@
-#!/bin/sh
-# Compare against latest. If you commit changes, replace "latest" directory
-# with "new" directory after running benchmark.
+#!/bin/bash
+# Run benchmark, comparing against previous result.
+
+set -e
 
 export RUSTFLAGS="-C target-cpu=native"
 
 cargo bench --bench bench_request_from_bytes -- --noplot --baseline latest
+
+read -p "Replace previous benchmark result with this one (y/N)? " answer
+
+case ${answer:0:1} in
+    y|Y )
+        cd aquatic_http_protocol/target/criterion/request-from-bytes/ &&
+        rm -r latest &&
+        mv new latest &&
+        echo "Replaced previous benchmark"
+    ;;
+esac
