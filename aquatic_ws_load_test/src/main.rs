e026a366f7b880b67e723068865501b583f40104
Joakim Frostegård <joakim.frostegard@gmail.com>
2020-08-01 06:42:27 +0200

    aquatic ws load test: fix cli program name

--- aquatic_ws_load_test/src/main.rs
+++ aquatic_ws_load_test/src/main.rs
@@ -21,7 +21,7 @@ static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
 pub fn main(){
     aquatic_cli_helpers::run_app_with_cli_and_config::<Config>(
-        "aquatic: udp bittorrent tracker: load tester",
+        "aquatic: webtorrent tracker load tester",
         run,
     )
 }
