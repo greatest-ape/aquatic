b3caa7ab7bc3c91b5a52dde41eca5fb23e6bcf61
Joakim Frostegård <joakim.frostegard@gmail.com>
2020-08-12 00:00:40 +0200

    Update TODO

--- scripts/run-aquatic-http.sh
+++ scripts/run-aquatic-http.sh
@@ -1,11 +1,5 @@
 #!/bin/sh 
 
-# Compile with target-cpu=native but without AVX512 features, since they
-# decrease performance.
-
-DISABLE_AVX512=$(rustc --print target-features | grep "    avx512" |
-    awk '{print $1}'  | sed 's/^/-C target-feature=-/' | xargs)
-
-export RUSTFLAGS="-C target-cpu=native $DISABLE_AVX512"
+. scripts/env-native-cpu-wihout-avx-512
 
 cargo run --release --bin aquatic_http -- $@
