13d0224321c7ac7aab13287c76e1c105f530c05d
Joakim Frostegård <joakim.frostegard@gmail.com>
2021-10-23 22:27:42 +0200

    aquatic_udp: use features "with-glommio" and (default) "with-mio"

--- aquatic_udp/src/lib/lib.rs
+++ aquatic_udp/src/lib/lib.rs
@@ -10,8 +10,9 @@ use cfg_if::cfg_if;
 
 pub mod common;
 pub mod config;
-#[cfg(all(feature = "io_uring", target_os = "linux"))]
+#[cfg(all(feature = "with-glommio", target_os = "linux"))]
 pub mod glommio;
+#[cfg(feature = "with-mio")]
 pub mod mio;
 
 use config::Config;
@@ -21,7 +22,7 @@ pub const APP_NAME: &str = "aquatic_udp: UDP BitTorrent tracker";
 
 pub fn run(config: Config) -> ::anyhow::Result<()> {
     cfg_if! {
-        if #[cfg(all(feature = "io_uring", target_os = "linux"))] {
+        if #[cfg(all(feature = "with-glommio", target_os = "linux"))] {
             glommio::run(config)
         } else {
             mio::run(config)
