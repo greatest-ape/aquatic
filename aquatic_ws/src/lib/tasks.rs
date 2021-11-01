dcf86d2df2ef6fd08c8cd41e3a11d461c841a671
Joakim Frostegård <joakim.frostegard@gmail.com>
2021-11-01 23:57:44 +0100

    aquatic_ws: remove unused statistics code and histogram dependency

--- aquatic_ws/src/lib/tasks.rs
+++ aquatic_ws/src/lib/tasks.rs
@@ -14,39 +14,3 @@ pub fn update_access_list(config: &Config, state: &State) {
         AccessListMode::Off => {}
     }
 }
-
-pub fn print_statistics(state: &State) {
-    let mut peers_per_torrent = Histogram::new();
-
-    {
-        let torrents = &mut state.torrent_maps.lock();
-
-        for torrent in torrents.ipv4.values() {
-            let num_peers = (torrent.num_seeders + torrent.num_leechers) as u64;
-
-            if let Err(err) = peers_per_torrent.increment(num_peers) {
-                eprintln!("error incrementing peers_per_torrent histogram: {}", err)
-            }
-        }
-        for torrent in torrents.ipv6.values() {
-            let num_peers = (torrent.num_seeders + torrent.num_leechers) as u64;
-
-            if let Err(err) = peers_per_torrent.increment(num_peers) {
-                eprintln!("error incrementing peers_per_torrent histogram: {}", err)
-            }
-        }
-    }
-
-    if peers_per_torrent.entries() != 0 {
-        println!(
-            "peers per torrent: min: {}, p50: {}, p75: {}, p90: {}, p99: {}, p999: {}, max: {}",
-            peers_per_torrent.minimum().unwrap(),
-            peers_per_torrent.percentile(50.0).unwrap(),
-            peers_per_torrent.percentile(75.0).unwrap(),
-            peers_per_torrent.percentile(90.0).unwrap(),
-            peers_per_torrent.percentile(99.0).unwrap(),
-            peers_per_torrent.percentile(99.9).unwrap(),
-            peers_per_torrent.maximum().unwrap(),
-        );
-    }
-}
