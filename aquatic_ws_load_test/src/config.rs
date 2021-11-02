814cd0e6d0b437a7e9753b9e855239de8367bd04
Joakim Frostegård <joakim.frostegard@gmail.com>
2021-11-02 16:34:22 +0100

    aquatic_ws_load_test: remove unused config fields

--- aquatic_ws_load_test/src/config.rs
+++ aquatic_ws_load_test/src/config.rs
@@ -9,19 +9,10 @@ pub struct Config {
     pub num_workers: u8,
     pub num_connections: usize,
     pub duration: usize,
-    pub network: NetworkConfig,
     pub torrents: TorrentConfig,
 }
 
 impl aquatic_cli_helpers::Config for Config {}
-
-#[derive(Clone, Debug, Serialize, Deserialize)]
-#[serde(default)]
-pub struct NetworkConfig {
-    pub connection_creation_interval: usize,
-    pub poll_timeout_microseconds: u64,
-    pub poll_event_capacity: usize,
-}
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 #[serde(default)]
@@ -49,18 +40,7 @@ impl Default for Config {
             num_workers: 1,
             num_connections: 16,
             duration: 0,
-            network: NetworkConfig::default(),
             torrents: TorrentConfig::default(),
-        }
-    }
-}
-
-impl Default for NetworkConfig {
-    fn default() -> Self {
-        Self {
-            connection_creation_interval: 40,
-            poll_timeout_microseconds: 1000,
-            poll_event_capacity: 4096,
         }
     }
 }
