1c8da337a1d8274592a0d7f59e3cb3998e745b06
Joakim Frostegård <joakim.frostegard@gmail.com>
2021-11-07 11:51:23 +0100

    scripts/run-aquatic-ws: support running either mio or glommio impl

--- scripts/run-aquatic-ws.sh
+++ scripts/run-aquatic-ws.sh
@@ -1,5 +1,14 @@
-#!/bin/sh 
+#!/bin/bash
 
 . ./scripts/env-native-cpu-without-avx-512
 
-cargo run --release --bin aquatic_ws -- $@
+if [[ -z $1 ]]; then
+    echo "Usage: $0 [mio|glommio]"
+else
+    if [ "$1" = "mio" ]; then
+        cargo run --release --bin aquatic_ws -- "${@:2}"
+    else
+        cargo run --release --features "with-glommio" --no-default-features --bin aquatic_ws -- "${@:2}"
+    fi
+fi
+
