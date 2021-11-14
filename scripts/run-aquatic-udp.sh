ce1c0b24c34ef45c041b3e08258a0a9eac4cd79c
Joakim Frostegård <joakim.frostegard@gmail.com>
2021-11-14 03:04:10 +0100

    udp: fix build failure; fix scripts/run-aquatic-udp.sh

--- scripts/run-aquatic-udp.sh
+++ scripts/run-aquatic-udp.sh
@@ -2,11 +2,13 @@
 
 . ./scripts/env-native-cpu-without-avx-512
 
-if [ "$1" != "mio" ] && [ "$1" != "glommio" ]; then
-    echo "Usage: $0 [mio|glommio] [ARGS]"
+if [ "$1" != "mio" ] && [ "$1" != "glommio" ] && [ "$1" != "io-uring" ]; then
+    echo "Usage: $0 [mio|glommio|io-uring] [ARGS]"
 else
     if [ "$1" = "mio" ]; then
         cargo run --release --bin aquatic_udp -- "${@:2}"
+    elif [ "$1" = "io-uring" ]; then
+        cargo run --release --features "with-io-uring" --no-default-features --bin aquatic_udp -- "${@:2}"
     else
         cargo run --release --features "with-glommio" --no-default-features --bin aquatic_udp -- "${@:2}"
     fi
