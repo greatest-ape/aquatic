c6a134683cea2751d8ee030f90637a431028b150
Joakim Frostegård <joakim.frostegard@gmail.com>
2023-01-18 21:50:10 +0100

    Toggle prometheus feature in scripts/run-aquatic-http.sh

--- scripts/run-aquatic-http.sh
+++ scripts/run-aquatic-http.sh
@@ -2,4 +2,4 @@
 
 . ./scripts/env-native-cpu-without-avx-512
 
-cargo run --profile "release-debug" -p aquatic_http -- $@
+cargo run --profile "release-debug" -p aquatic_http --features "prometheus" -- $@
