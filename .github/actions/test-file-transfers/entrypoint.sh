206b0d81b2de9223c20dd9020f13ea55ff202c30
Joakim Frostegård <joakim.frostegard@gmail.com>
2024-05-03 00:07:25 +0200

    transfer ci ssl fix

--- .github/actions/test-file-transfers/entrypoint.sh
+++ .github/actions/test-file-transfers/entrypoint.sh
@@ -40,11 +40,11 @@ fi
 
 $SUDO echo "127.0.0.1    example.com" >> /etc/hosts
 
-openssl ecparam -genkey -name prime256v1 -out ca.key
+openssl genrsa -out ca.key 2048
 openssl req -new -x509 -days 365 -key ca.key -subj "/C=CN/ST=GD/L=SZ/O=Acme, Inc./CN=Acme Root CA" -out ca.crt
-openssl req -newkey prime256v1 -nodes -keyout server.key -subj "/C=CN/ST=GD/L=SZ/O=Acme, Inc./CN=*.example.com" -out server.csr
+openssl req -newkey rsa:2048 -nodes -keyout server.key -subj "/C=CN/ST=GD/L=SZ/O=Acme, Inc./CN=*.example.com" -out server.csr
 openssl x509 -req -extfile <(printf "subjectAltName=DNS:example.com,DNS:www.example.com") -days 365 -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out server.crt
-openssl pkcs8 -in ca.key -topk8 -nocrypt -out key.pk8
+openssl pkcs8 -in server.key -topk8 -nocrypt -out key.pk8
 
 $SUDO cp ca.crt /usr/local/share/ca-certificates/snakeoil-ca.crt
 $SUDO cp server.crt /usr/local/share/ca-certificates/snakeoil-server.crt
