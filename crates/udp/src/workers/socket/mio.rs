638897f8e277982e6fd350de34626505deb9cd48
Joakim Frostegård <joakim.frostegard@gmail.com>
2025-01-14 22:29:17 +0100

    tmp work on udp double sockets

--- crates/udp/src/workers/socket/mio.rs
+++ crates/udp/src/workers/socket/mio.rs
@@ -1,4 +1,5 @@
 use std::io::{Cursor, ErrorKind};
+use std::marker::PhantomData;
 use std::sync::atomic::Ordering;
 use std::time::Duration;
 
@@ -22,18 +23,7 @@ use crate::config::Config;
 use super::validator::ConnectionValidator;
 use super::{create_socket, EXTRA_PACKET_SIZE_IPV4, EXTRA_PACKET_SIZE_IPV6};
 
-pub struct SocketWorker {
-    config: Config,
-    shared_state: State,
-    statistics: CachePaddedArc<IpVersionStatistics<SocketWorkerStatistics>>,
-    statistics_sender: Sender<StatisticsMessage>,
-    access_list_cache: AccessListCache,
-    validator: ConnectionValidator,
-    socket: UdpSocket,
-    buffer: [u8; BUFFER_SIZE],
-    rng: SmallRng,
-    peer_valid_until: ValidUntil,
-}
+struct SocketWorker;
 
 impl SocketWorker {
     pub fn run(
@@ -51,25 +41,20 @@ impl SocketWorker {
             config.cleaning.max_peer_age,
         );
 
-        let mut worker = Self {
+        let mut shared = WorkerSharedData {
             config,
             shared_state,
             statistics,
             statistics_sender,
             validator,
             access_list_cache,
-            socket,
             buffer: [0; BUFFER_SIZE],
             rng: SmallRng::from_entropy(),
             peer_valid_until,
         };
 
-        worker.run_inner()
-    }
-
-    pub fn run_inner(&mut self) -> anyhow::Result<()> {
         let mut opt_resend_buffer =
-            (self.config.network.resend_buffer_max_len > 0).then_some(Vec::new());
+            (shared.config.network.resend_buffer_max_len > 0).then_some(Vec::new());
         let mut events = Events::with_capacity(1);
         let mut poll = Poll::new().context("create poll")?;
 
@@ -90,109 +75,35 @@ impl SocketWorker {
                 }
             }
 
-            // If resend buffer is enabled, send any responses in it
-            if let Some(resend_buffer) = opt_resend_buffer.as_mut() {
-                for (addr, response) in resend_buffer.drain(..) {
-                    self.send_response(&mut None, addr, response);
-                }
-            }
+            // FIXME: resend buffer
 
             if iter_counter % 256 == 0 {
-                self.validator.update_elapsed();
+                shared.validator.update_elapsed();
 
-                self.peer_valid_until = ValidUntil::new(
-                    self.shared_state.server_start_instant,
-                    self.config.cleaning.max_peer_age,
+                shared.peer_valid_until = ValidUntil::new(
+                    shared.shared_state.server_start_instant,
+                    shared.config.cleaning.max_peer_age,
                 );
             }
 
             iter_counter = iter_counter.wrapping_add(1);
         }
     }
+}
 
-    fn read_and_handle_requests(
-        &mut self,
-        opt_resend_buffer: &mut Option<Vec<(CanonicalSocketAddr, Response)>>,
-    ) {
-        let max_scrape_torrents = self.config.protocol.max_scrape_torrents;
+struct WorkerSharedData {
+    config: Config,
+    shared_state: State,
+    statistics: CachePaddedArc<IpVersionStatistics<SocketWorkerStatistics>>,
+    statistics_sender: Sender<StatisticsMessage>,
+    access_list_cache: AccessListCache,
+    validator: ConnectionValidator,
+    buffer: [u8; BUFFER_SIZE],
+    rng: SmallRng,
+    peer_valid_until: ValidUntil,
+}
 
-        loop {
-            match self.socket.recv_from(&mut self.buffer[..]) {
-                Ok((bytes_read, src)) => {
-                    let src_port = src.port();
-                    let src = CanonicalSocketAddr::new(src);
-
-                    // Use canonical address for statistics
-                    let opt_statistics = if self.config.statistics.active() {
-                        if src.is_ipv4() {
-                            let statistics = &self.statistics.ipv4;
-
-                            statistics
-                                .bytes_received
-                                .fetch_add(bytes_read + EXTRA_PACKET_SIZE_IPV4, Ordering::Relaxed);
-
-                            Some(statistics)
-                        } else {
-                            let statistics = &self.statistics.ipv6;
-
-                            statistics
-                                .bytes_received
-                                .fetch_add(bytes_read + EXTRA_PACKET_SIZE_IPV6, Ordering::Relaxed);
-
-                            Some(statistics)
-                        }
-                    } else {
-                        None
-                    };
-
-                    if src_port == 0 {
-                        ::log::debug!("Ignored request because source port is zero");
-
-                        continue;
-                    }
-
-                    match Request::parse_bytes(&self.buffer[..bytes_read], max_scrape_torrents) {
-                        Ok(request) => {
-                            if let Some(statistics) = opt_statistics {
-                                statistics.requests.fetch_add(1, Ordering::Relaxed);
-                            }
-
-                            if let Some(response) = self.handle_request(request, src) {
-                                self.send_response(opt_resend_buffer, src, response);
-                            }
-                        }
-                        Err(RequestParseError::Sendable {
-                            connection_id,
-                            transaction_id,
-                            err,
-                        }) if self.validator.connection_id_valid(src, connection_id) => {
-                            let response = ErrorResponse {
-                                transaction_id,
-                                message: err.into(),
-                            };
-
-                            self.send_response(opt_resend_buffer, src, Response::Error(response));
-
-                            ::log::debug!("request parse error (sent error response): {:?}", err);
-                        }
-                        Err(err) => {
-                            ::log::debug!(
-                                "request parse error (didn't send error response): {:?}",
-                                err
-                            );
-                        }
-                    };
-                }
-                Err(err) if err.kind() == ErrorKind::WouldBlock => {
-                    break;
-                }
-                Err(err) => {
-                    ::log::warn!("recv_from error: {:#}", err);
-                }
-            }
-        }
-    }
-
+impl WorkerSharedData {
     fn handle_request(&mut self, request: Request, src: CanonicalSocketAddr) -> Option<Response> {
         let access_list_mode = self.config.access_list.mode;
 
@@ -245,14 +156,107 @@ impl SocketWorker {
 
         None
     }
+}
 
+struct IPV4;
+struct IPV6;
+
+struct Socket<T> {
+    socket: UdpSocket,
+    opt_resend_buffer: Option<Vec<(CanonicalSocketAddr, Response)>>,
+    phantom_data: PhantomData<T>,
+}
+
+impl Socket<IPV4> {
+    fn read_and_handle_requests(&mut self
+    , shared: &mut WorkerSharedData
+    ) {
+        let max_scrape_torrents = shared.config.protocol.max_scrape_torrents;
+
+        loop {
+            match self.socket.recv_from(&mut shared.buffer[..]) {
+                Ok((bytes_read, src)) => {
+                    let src_port = src.port();
+                    let src = CanonicalSocketAddr::new(src);
+
+                    // Use canonical address for statistics
+                    let opt_statistics = if shared.config.statistics.active() {
+                        if src.is_ipv4() {
+                            let statistics = &shared.statistics.ipv4;
+
+                            statistics
+                                .bytes_received
+                                .fetch_add(bytes_read + EXTRA_PACKET_SIZE_IPV4, Ordering::Relaxed);
+
+                            Some(statistics)
+                        } else {
+                            let statistics = &shared.statistics.ipv6;
+
+                            statistics
+                                .bytes_received
+                                .fetch_add(bytes_read + EXTRA_PACKET_SIZE_IPV6, Ordering::Relaxed);
+
+                            Some(statistics)
+                        }
+                    } else {
+                        None
+                    };
+
+                    if src_port == 0 {
+                        ::log::debug!("Ignored request because source port is zero");
+
+                        continue;
+                    }
+
+                    match Request::parse_bytes(&shared.buffer[..bytes_read], max_scrape_torrents) {
+                        Ok(request) => {
+                            if let Some(statistics) = opt_statistics {
+                                statistics.requests.fetch_add(1, Ordering::Relaxed);
+                            }
+
+                            if let Some(response) = shared.handle_request(request, src) {
+                                self.send_response (shared, src, response, false);
+                            }
+                        }
+                        Err(RequestParseError::Sendable {
+                            connection_id,
+                            transaction_id,
+                            err,
+                        }) if shared.validator.connection_id_valid(src, connection_id) => {
+                            let response = ErrorResponse {
+                                transaction_id,
+                                message: err.into(),
+                            };
+
+                            self.send_response( shared, src, Response::Error(response), false);
+
+                            ::log::debug!("request parse error (sent error response): {:?}", err);
+                        }
+                        Err(err) => {
+                            ::log::debug!(
+                                "request parse error (didn't send error response): {:?}",
+                                err
+                            );
+                        }
+                    };
+                }
+                Err(err) if err.kind() == ErrorKind::WouldBlock => {
+                    break;
+                }
+                Err(err) => {
+                    ::log::warn!("recv_from error: {:#}", err);
+                }
+            }
+        }
+    }
     fn send_response(
         &mut self,
-        opt_resend_buffer: &mut Option<Vec<(CanonicalSocketAddr, Response)>>,
+        shared: &mut WorkerSharedData,
         canonical_addr: CanonicalSocketAddr,
         response: Response,
+        disable_resend_buffer: bool,
     ) {
-        let mut buffer = Cursor::new(&mut self.buffer[..]);
+        let mut buffer = Cursor::new(&mut shared.buffer[..]);
 
         if let Err(err) = response.write_bytes(&mut buffer) {
             ::log::error!("failed writing response to buffer: {:#}", err);
@@ -262,7 +266,7 @@ impl SocketWorker {
 
         let bytes_written = buffer.position() as usize;
 
-        let addr = if self.config.network.address.is_ipv4() {
+        let addr = if shared.config.network.address.is_ipv4() {
             canonical_addr
                 .get_ipv4()
                 .expect("found peer ipv6 address while running bound to ipv4 address")
@@ -274,9 +278,9 @@ impl SocketWorker {
             .socket
             .send_to(&buffer.into_inner()[..bytes_written], addr)
         {
-            Ok(bytes_sent) if self.config.statistics.active() => {
+            Ok(bytes_sent) if shared.config.statistics.active() => {
                 let stats = if canonical_addr.is_ipv4() {
-                    let stats = &self.statistics.ipv4;
+                    let stats = &shared.statistics.ipv4;
 
                     stats
                         .bytes_sent
@@ -284,7 +288,7 @@ impl SocketWorker {
 
                     stats
                 } else {
-                    let stats = &self.statistics.ipv6;
+                    let stats = &shared.statistics.ipv6;
 
                     stats
                         .bytes_sent
@@ -309,12 +313,12 @@ impl SocketWorker {
                 }
             }
             Ok(_) => (),
-            Err(err) => match opt_resend_buffer.as_mut() {
+            Err(err) => match self.opt_resend_buffer.as_mut() {
                 Some(resend_buffer)
-                    if (err.raw_os_error() == Some(libc::ENOBUFS))
+                    if !disable_resend_buffer && (err.raw_os_error() == Some(libc::ENOBUFS))
                         || (err.kind() == ErrorKind::WouldBlock) =>
                 {
-                    if resend_buffer.len() < self.config.network.resend_buffer_max_len {
+                    if resend_buffer.len() < shared.config.network.resend_buffer_max_len {
                         ::log::debug!("Adding response to resend queue, since sending it to {} failed with: {:#}", addr, err);
 
                         resend_buffer.push((canonical_addr, response));
@@ -329,5 +333,28 @@ impl SocketWorker {
         }
 
         ::log::debug!("send response fn finished");
+    }
+
+
+    /// If resend buffer is enabled, send any responses in it
+    fn resend_failed(&mut self, shared: &mut WorkerSharedData) {
+        if self.opt_resend_buffer.is_some() {
+            let mut tmp_resend_buffer = Vec::new();
+
+            // Do memory swap shenanigans to get around false positive in
+            // borrow checker regarding double mut borrowing of self
+
+            if let Some(resend_buffer) = self.opt_resend_buffer.as_mut() {
+                ::std::mem::swap(resend_buffer, &mut tmp_resend_buffer);
+            }
+
+            for (addr, response) in tmp_resend_buffer.drain(..) {
+                self.send_response(shared, addr, response, true);
+            }
+
+            if let Some(resend_buffer) = self.opt_resend_buffer.as_mut() {
+                ::std::mem::swap(resend_buffer, &mut tmp_resend_buffer);
+            }
+        }
     }
 }
